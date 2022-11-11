8cb78f0ec67163cff486ded65a6018ea09d9af43
Moirangthem Gelson Singh <37416550+gelsonm@users.noreply.github.com>
2022-11-11 10:59:00 +0530

    Time: 541 ms (50.03%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,13 +1,25 @@
 # Write your MySQL query statement below
 
-select id,'Root' as type
-from Tree
-where p_id is Null
-union
-select id,'Inner' as type
-from Tree
-where id in (select p_id from Tree) and p_id is not Null
-union
-select id,'Leaf' as type
-from Tree
-where id not in (select p_id from Tree where p_id is not Null) and p_id is not Null
+# Approach 1
+# select id,'Root' as type
+# from Tree
+# where p_id is Null
+# union
+# select id,'Inner' as type
+# from Tree
+# where id in (select p_id from Tree) and p_id is not Null
+# union
+# select id,'Leaf' as type
+# from Tree
+# where id not in (select p_id from Tree where p_id is not Null) and p_id is not Null
+
+# Approach 2
+SELECT id,
+    CASE 
+        WHEN p_id is NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+        ELSE 'Leaf'
+        END AS type
+        
+FROM TREE
+
