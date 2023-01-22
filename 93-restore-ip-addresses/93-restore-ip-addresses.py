0008c798ad145bb9832850711e766d7288e6e3ff
Moirangthem Gelson Singh <37416550+gelsonm@users.noreply.github.com>
2023-01-22 06:24:08 +0530

    Time: 51 ms (49.54%), Space: 14 MB (31.67%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -1,18 +1,32 @@
 class Solution:
+    def valid(self, s: str, start: int, length: int) -> bool:
+        return length == 1 or (s[start] != '0' and (length < 3 or s[start:start+length] <= "255"))
+
+    def helper(self, s: str, startIndex: int, dots: List[int], ans: List[str]) -> None:
+        remainingLength = len(s) - startIndex
+        remainingNumberOfIntegers = 4 - len(dots)
+
+        if remainingLength > remainingNumberOfIntegers * 3 or remainingLength < remainingNumberOfIntegers:
+            return
+        if len(dots) == 3:
+            if self.valid(s, startIndex, remainingLength):
+                temp = ""
+                last = 0
+                for dot in dots:
+                    temp += s[last:last+dot]
+                    last += dot
+                    temp += "."
+                temp += s[startIndex:]
+                ans.append(temp)
+            return
+        for curPos in range(1, min(4, remainingLength+1)):
+            dots.append(curPos)
+            if self.valid(s, startIndex, curPos):
+                self.helper(s, startIndex + curPos, dots, ans)
+            dots.pop()
+
     def restoreIpAddresses(self, s: str) -> List[str]:
-        def backtrack(start):
-            if len(octets) == 4 and start == len(s):
-                ips.append('.'.join(octets))
-                return
-            for size in range(1, 4):
-                octet = s[start:start + size]
-                if len(octet) > 1 and (octet[0] == '0' or int(octet) > 255):
-                    continue
-                if len(octets) < 4:
-                    octets.append(octet)
-                    backtrack(start + size)
-                    octets.pop()
-        octets = []
-        ips = []
-        backtrack(0)
-        return ips
+        dots = []
+        ans = []
+        self.helper(s, 0, dots, ans)
+        return ans
