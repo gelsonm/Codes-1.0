e4ee2358785312fe5d36bed908eb7e5458477134
Moirangthem Gelson Singh <37416550+gelsonm@users.noreply.github.com>
2023-01-23 21:57:40 +0530

    Time: 180 ms (45.86%), Space: 16.7 MB (47.17%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,12 +1,21 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Brute force
-        freq = {}
+        "'''"
+#         freq = {}
         
-        for num in nums:
-            freq[num] = freq.get(num,0) + 1
+#         for num in nums:
+#             freq[num] = freq.get(num,0) + 1
             
-        for num in freq:
-            if freq[num] == 1:
-                return num
+#         for num in freq:
+#             if freq[num] == 1:
+#                 return num
+        "'''"
+    
+        # XOR bits
+        ans = nums[0]
         
+        for num in nums[1:]:
+            ans ^= num
+            
+        return ans
