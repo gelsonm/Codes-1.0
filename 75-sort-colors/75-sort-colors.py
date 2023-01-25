5ea5b45cbf26bd0c9a8c351efd2c0bbcea3e1b2b
Moirangthem Gelson Singh <37416550+gelsonm@users.noreply.github.com>
2023-01-25 18:52:29 +0530

    Time: 51 ms (32.16%), Space: 13.9 MB (57.17%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,6 +4,26 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
+        # Brute Force
+        # Initialize counters for 0s, 1s, and 2s
+        zero_count = one_count = two_count = 0
+        # Count the number of 0s, 1s, and 2s
+        for num in nums:
+            if num == 0:
+                zero_count += 1
+            elif num == 1:
+                one_count += 1
+            else:
+                two_count += 1
+        # Overwrite the array with the sorted values
+        for i in range(zero_count):
+            nums[i] = 0
+        for i in range(zero_count, zero_count + one_count):
+            nums[i] = 1
+        for i in range(zero_count + one_count, len(nums)):
+            nums[i] = 2
+            
+            
         # Dutch National Flag algorithm
         low = 0
         mid = 0
