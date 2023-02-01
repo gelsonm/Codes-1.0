f4927be060b9594a3d14d98b4d3cc5623cad8786
Moirangthem Gelson Singh <37416550+gelsonm@users.noreply.github.com>
2023-02-01 07:18:43 +0530

    Time: 35 ms (70.52%), Space: 13.9 MB (70.81%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        len1 = min(len(str1), len(str2))
+        for i in range(len1, 0, -1):
+            if len(str1) % i == 0 and len(str2) % i == 0:
+                sub1 = str1[:i]
+                sub2 = str2[:i]
+                if sub1 * (len(str1) // i) == str1 and sub1 * (len(str2) // i) == str2:
+                    return sub1
+        return ""
